0f1aba3fd1ab9bf8d70ed809b9b382da6cb80c2e
Erasylbruh <igrok.20062003@gmail.com>
2025-11-26 22:17:47 +0500

    db changes

--- server/index.js
+++ server/index.js
@@ -22,7 +22,11 @@ const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'karaoke_animator'
+    database: process.env.DB_NAME || 'karaoke_animator',
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key';
